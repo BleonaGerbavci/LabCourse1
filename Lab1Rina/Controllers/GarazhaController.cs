52a63f42f0f12f325c4e4de01215d356ff03b21a
rinashkodra <rs52548@ubt-uni.net>
2022-04-30 18:43:37 +0200

    Garazha and Pompa CRUD-s(backend & database)

---
+++ Lab1Rina/Controllers/GarazhaController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab1Rina.Data;
+using Lab1Rina.Models;
+
+namespace Lab1Rina.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GarazhaController : ControllerBase
+    {
+        private readonly DB_Bus_SystemContext _context;
+
+        public GarazhaController(DB_Bus_SystemContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Garazha>>> Get()
+        {
+            return Ok(await _context.Garazha.ToListAsync());
+        }
+
+
+
+        //Get a single Garages by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Garazha>>> Get(int id)
+        {
+            var garazha = await _context.Garazha.FindAsync(id);
+            if (garazha == null)
+                return BadRequest("Garazha not found.");
+            return Ok(garazha);
+        }
+
+
+        //Create a Garages 
+        [HttpPost]
+        public async Task<ActionResult<List<Garazha>>> AddGarazha(Garazha garazha)
+        {
+            _context.Garazha.Add(garazha);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Garazha.ToListAsync());
+        }
+
+        //Update Garages
+        [HttpPut]
+        public async Task<ActionResult<Garazha>> UpdateGarazha(Garazha request)
+        {
+            var dbgarazha = await _context.Garazha.FindAsync(request.Id);
+            if (dbgarazha == null)
+                return BadRequest("garazha not found");
+
+            dbgarazha.StreetName = request.StreetName;
+            dbgarazha.City = request.City;
+           
+
+
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Garazha.ToListAsync());
+        }
+
+        //Delete a Garages
+        [HttpDelete]
+        public async Task<ActionResult<List<Garazha>>> DeleteGarazha(int id)
+        {
+            var dbgarazha = await _context.Garazha.FindAsync(id);
+            if (dbgarazha == null)
+                return BadRequest("Garazha not found");
+
+            _context.Garazha.Remove(dbgarazha);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Garazha.ToListAsync());
+        }
+    }
+}
---
+++ Lab1Rina/Program.cs
@@ -0,0 +1,35 @@
+global using Microsoft.EntityFrameworkCore;
+using Lab1Rina.Data;
+using Lab1Rina.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DB_Bus_SystemContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBconnection"));
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
